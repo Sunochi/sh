e7309f85d2cb378f3035860ccd10b107fbaef247
T.Sunochi <s01718@CG4536.local>
2018-05-22 09:57:51 +0900

    バグの修正

--- sekigae.sh
+++ sekigae.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-array_name=("田中1" "田中2" "田中3" "田中4" "田中5" "田中6" "田中7" "田中8" "田中9" "田中10")
+array_name=("田中" "田中" "田中" "田中" "田中" "田中" "田中" "田中" "田中" "田中")
 array_seki=("左前左" "左前右" "左後左" "左後右" "右前左" "右前中" "右前右" "右後左" "右後中" "右後右")
 array_count=(0 0 0 0 0 0 0 0 0 0)
 
 nums=${#array_name[*]}
+j=0
 
 function changeAll() {
     while :
@@ -11,10 +12,11 @@ function changeAll() {
         random=$((RANDOM%$nums))
         if [ $(( array_count[$random]+1 )) -eq 1 ]
         then
-            name=${array_name[$random]}
+            name=${array_name[$i]}
             seki=${array_seki[$random]}
             echo $seki : $name
             array_count[$random]=$(( array_count[$random]+1 ))
+            j=$((i+1))
             break
         fi
     done
